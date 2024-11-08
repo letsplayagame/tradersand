cfb4686dbcfa08f0b1c825cd4b415c6780e8f9df
DrJackHammer <kaangonel@gmail.com>
2024-11-08 07:52:07 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -20,3 +20,35 @@ self.addEventListener('install', event => {
             })
     );
 });
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event - serve from cache, fall back to network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request)
+                    .then(fetchResponse => {
+                        // Cache the fetched response
+                        return caches.open(CACHE_NAME).then(cache => {
+                            cache.put(event.request, fetchResponse.clone());
+                            return fetchResponse;
+                        });
+                    });
+            })
+    );
+});
